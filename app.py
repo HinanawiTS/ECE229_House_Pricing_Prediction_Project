08ef6dfd89d1162035b0ff048f7bfbd181785ac0
changliu020 <changliu020@gmail.com>
2022-05-24 01:26:43 -0700

    docker setup

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 def load_model():
     global model
     # model variable refers to the global variable
-    with open('iris_trained_model.pkl', 'rb') as f:
+    with open('model.pkl', 'rb') as f:
         model = pickle.load(f)
 
 
@@ -24,12 +24,13 @@ def home_endpoint():
 def get_prediction():
     # Works only for a single sample
     if request.method == 'POST':
-        data = request.get_json()  # Get data posted as a json
+        data_json = request.get_json()  # Get data posted as a json
+        data = data_json['data']
         data = np.array(data)[np.newaxis, :]  # converts shape from (4,) to (1, 4)
         prediction = model.predict(data)  # runs globally loaded model on the data
     return str(prediction[0])
 
 
 if __name__ == '__main__':
-    # load_model()  # load model at the beginning once only
+    load_model()  # load model at the beginning once only
     app.run(host='0.0.0.0', port=5000)
