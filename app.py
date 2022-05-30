1dd68355e930b215b0ee15b6909efa09ba6700fb
changliu020 <changliu020@gmail.com>
2022-05-30 00:26:09 -0700

    bokeh work

--- app.py
+++ app.py
@@ -68,6 +68,7 @@ def home_endpoint():
     neighbourhoodGroupSet = set(df['neighbourhood_group'])
     neighbourhoodSet = set(df['neighbourhood'])
 
+    # select data according to the submitted form
     if request.method == 'POST':
         result = request.form
         attributes = []
@@ -77,12 +78,12 @@ def home_endpoint():
         if roomTypeList != []:
             attributes.append('room_type')
             ranges.append(roomTypeList)
-        
+
         neighbourhoodGroupList = result.getlist('neighbourhoodGroup')
         if neighbourhoodGroupList != []:
             attributes.append('neighbourhood_group')
             ranges.append(neighbourhoodGroupList)
-        
+
         priceRangeList = result.getlist('priceRange')
         if priceRangeList != []:
             attributes.append('price')
@@ -99,19 +100,10 @@ def home_endpoint():
             ranges.append(int(min_reviews))
         df_selected = select(df, attributes, ranges)
 
-    return render_template('index.html',
-                           tables=[df_selected.head().to_html(
-                               classes='data', header='true')],
-                           roomTypeSet=roomTypeSet,
-                           neighbourhoodGroupSet=neighbourhoodGroupSet,
-                           neighbourhoodSet=neighbourhoodSet)
-
-
-@app.route('/')
-def plot_bokeh_smalldf():
     ###ADDED####
     dataframe = pd.read_csv('./data/final_dataframe.csv', index_col=0)
-    room_list = ['bedroom', 'bedrooms', 'bed', 'beds', 'bdrs', 'bdr', 'room', 'rooms']
+    room_list = ['bedroom', 'bedrooms', 'bed',
+                 'beds', 'bdrs', 'bdr', 'room', 'rooms']
 
     def sort_keys(ls, df):
         all_room_df = df[df.title_split.str.contains('|'.join(ls))]
@@ -124,7 +116,8 @@ def plot_bokeh_smalldf():
     palette = Category20c[10]  # PRGn[11]
     source = ColumnDataSource(data=df)
     # Define color mapper - which column will define the colour of the data points
-    color_mapper = linear_cmap(field_name='price', palette=palette, low=df['price'].min(), high=df['price'].max())
+    color_mapper = linear_cmap(
+        field_name='price', palette=palette, low=df['price'].min(), high=df['price'].max())
 
     # Set tooltips - these appear when we hover over a data point in our map, very nifty and very useful
     tooltips = [("Price", "@price"), ("Region", "@neighbourhood"), ("Keywords", "@title_split"),
@@ -135,19 +128,69 @@ def plot_bokeh_smalldf():
     # Add map tile
     p.add_tile(chosentile)
     # Add points using mercator coordinates
-    p.circle(x='mercator_x', y='mercator_y', color=color_mapper, source=source, size=3, fill_alpha=0.9)
+    p.circle(x='mercator_x', y='mercator_y', color=color_mapper,
+             source=source, size=3, fill_alpha=0.9)
     # Defines color bar
     color_bar = ColorBar(color_mapper=color_mapper['transform'],
                          formatter=NumeralTickFormatter(format='0.0[0000]'),
                          label_standoff=13, width=8, location=(0, 0))
     p.add_layout(color_bar, 'right')
-#     output_notebook()
-#     show(p)
-#     print(len(components(p)))
-    script1, div1= components(p)
-    cdn_js=CDN.js_files[0]
-    cdn_css=CDN.css_files[0]
-    return render_template("index.html", script1=script1,div1=div1,cdn_css=cdn_css,cdn_js=cdn_js)
+    # output_notebook()
+    # show(p)
+    # print(len(components(p)))
+    script1, div1 = components(p)
+    cdn_js = CDN.js_files[0]
+    # cdn_css = CDN.css_files[0]
+
+    return render_template('index.html',
+                           tables=[df_selected.head().to_html(
+                               classes='data', header='true')],
+                           roomTypeSet=roomTypeSet,
+                           neighbourhoodGroupSet=neighbourhoodGroupSet,
+                           neighbourhoodSet=neighbourhoodSet,
+                           script1=script1, div1=div1, cdn_js=cdn_js)
+
+# @app.route('/')
+# def plot_bokeh_smalldf():
+#     ###ADDED####
+#     dataframe = pd.read_csv('./data/final_dataframe.csv', index_col=0)
+#     room_list = ['bedroom', 'bedrooms', 'bed', 'beds', 'bdrs', 'bdr', 'room', 'rooms']
+
+#     def sort_keys(ls, df):
+#         all_room_df = df[df.title_split.str.contains('|'.join(ls))]
+#         return all_room_df
+
+#     df = sort_keys(room_list, dataframe)
+#     ###ADDED####
+
+#     chosentile = get_provider(Vendors.OSM)
+#     palette = Category20c[10]  # PRGn[11]
+#     source = ColumnDataSource(data=df)
+#     # Define color mapper - which column will define the colour of the data points
+#     color_mapper = linear_cmap(field_name='price', palette=palette, low=df['price'].min(), high=df['price'].max())
+
+#     # Set tooltips - these appear when we hover over a data point in our map, very nifty and very useful
+#     tooltips = [("Price", "@price"), ("Region", "@neighbourhood"), ("Keywords", "@title_split"),
+#                 ("Number of Reviews", "@number_of_reviews")]
+#     # Create figure
+#     p = figure(title='Airbnb price by region in NYC', x_axis_type="mercator", y_axis_type="mercator",
+#                x_axis_label='Longitude', y_axis_label='Latitude', tooltips=tooltips)
+#     # Add map tile
+#     p.add_tile(chosentile)
+#     # Add points using mercator coordinates
+#     p.circle(x='mercator_x', y='mercator_y', color=color_mapper, source=source, size=3, fill_alpha=0.9)
+#     # Defines color bar
+#     color_bar = ColorBar(color_mapper=color_mapper['transform'],
+#                          formatter=NumeralTickFormatter(format='0.0[0000]'),
+#                          label_standoff=13, width=8, location=(0, 0))
+#     p.add_layout(color_bar, 'right')
+# #     output_notebook()
+# #     show(p)
+# #     print(len(components(p)))
+#     script1, div1= components(p)
+#     cdn_js=CDN.js_files[0]
+#     cdn_css=CDN.css_files[0]
+#     return render_template("index.html", script1=script1,div1=div1,cdn_css=cdn_css,cdn_js=cdn_js)
 
 
 # @app.route('/predict', methods=['POST'])
