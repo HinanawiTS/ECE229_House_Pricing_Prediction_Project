edab928795a6f8f67fe73b4bc701b987ffb14aa3
changliu020 <changliu020@gmail.com>
2022-05-30 20:44:59 -0700

    neighbourhood selection done

--- application.py
+++ application.py
@@ -37,6 +37,18 @@ def select(df, attributes, ranges):
     return selected_df
 
 
+def select_ng_dict(df):
+    ng_dict = {}
+    for _, row in df.iterrows():
+        ng = row['neighbourhood_group']
+        if ng not in ng_dict.keys():
+            ng_dict[ng] = set()
+        ng_dict[ng].add(row['neighbourhood'])
+
+    for key, val in ng_dict.items():
+        ng_dict[key] = list(val)
+    return ng_dict
+
 def load_model():
     global model
     # model variable refers to the global variable
@@ -59,47 +71,38 @@ def parse_price_range(priceRangeStrList):
     return priceRangeList
 
 
-@application.route('/', methods=['POST', 'GET'])
-def home_endpoint():
-    global df
-    df = get_pd_df()
-    df_selected = df.copy(deep=True)
-    roomTypeSet = set(df['room_type'])
-    neighbourhoodGroupSet = set(df['neighbourhood_group'])
-    neighbourhoodSet = set(df['neighbourhood'])
+def select_from_request(result):
+    attributes = []
+    ranges = []
 
-    # select data according to the submitted form
-    if request.method == 'POST':
-        result = request.form
-        attributes = []
-        ranges = []
+    roomTypeList = result.getlist('roomType')
+    if roomTypeList != []:
+        attributes.append('room_type')
+        ranges.append(roomTypeList)
 
-        roomTypeList = result.getlist('roomType')
-        if roomTypeList != []:
-            attributes.append('room_type')
-            ranges.append(roomTypeList)
+    neighbourhoodGroupList = result.getlist('neighbourhoodGroup')
+    if neighbourhoodGroupList != []:
+        attributes.append('neighbourhood_group')
+        ranges.append(neighbourhoodGroupList)
 
-        neighbourhoodGroupList = result.getlist('neighbourhoodGroup')
-        if neighbourhoodGroupList != []:
-            attributes.append('neighbourhood_group')
-            ranges.append(neighbourhoodGroupList)
+    priceRangeList = result.getlist('priceRange')
+    if priceRangeList != []:
+        attributes.append('price')
+        ranges.append(parse_price_range(priceRangeList))
 
-        priceRangeList = result.getlist('priceRange')
-        if priceRangeList != []:
-            attributes.append('price')
-            ranges.append(parse_price_range(priceRangeList))
+    min_nights = result.get('minNight')
+    if min_nights:
+        attributes.append('minimum_nights')
+        ranges.append(int(min_nights))
 
-        min_nights = result.get('minNight')
-        if min_nights:
-            attributes.append('minimum_nights')
-            ranges.append(int(min_nights))
+    min_reviews = result.get('minReview')
+    if min_reviews:
+        attributes.append('number_of_reviews')
+        ranges.append(int(min_reviews))
+    return select(df, attributes, ranges)
 
-        min_reviews = result.get('minReview')
-        if min_reviews:
-            attributes.append('number_of_reviews')
-            ranges.append(int(min_reviews))
-        df_selected = select(df, attributes, ranges)
 
+def plot_bokeh_map():
     ###ADDED####
     dataframe = pd.read_csv('./data/final_dataframe.csv', index_col=0)
     room_list = ['bedroom', 'bedrooms', 'bed',
@@ -141,56 +144,33 @@ def home_endpoint():
     script1, div1 = components(p)
     cdn_js = CDN.js_files[0]
     # cdn_css = CDN.css_files[0]
+    return script1, div1, cdn_js
+
+
+@application.route('/', methods=['POST', 'GET'])
+def home_endpoint():
+    global df
+    df = get_pd_df()
+    df_selected = df.copy(deep=True)
+    roomTypeSet = set(df['room_type'])
+    neighbourhoodGroupSet = set(df['neighbourhood_group'])
+    neighbourhoodSet = set(df['neighbourhood'])
+
+    ng_dict = select_ng_dict(df)
+
+    script1, div1, cdn_js = plot_bokeh_map()
+
+    # select data according to the submitted form
+    if request.method == 'POST':
+        df_selected = select_from_request(request.form)
 
     return render_template('index.html',
                            tables=[df_selected.head().to_html(
                                classes='data', header='true')],
                            roomTypeSet=roomTypeSet,
                            neighbourhoodGroupSet=neighbourhoodGroupSet,
-                           neighbourhoodSet=neighbourhoodSet,
+                           neighbourhoodSet=neighbourhoodSet, ng_dict=ng_dict,
                            script1=script1, div1=div1, cdn_js=cdn_js)
-
-# @application.route('/')
-# def plot_bokeh_smalldf():
-#     ###ADDED####
-#     dataframe = pd.read_csv('./data/final_dataframe.csv', index_col=0)
-#     room_list = ['bedroom', 'bedrooms', 'bed', 'beds', 'bdrs', 'bdr', 'room', 'rooms']
-
-#     def sort_keys(ls, df):
-#         all_room_df = df[df.title_split.str.contains('|'.join(ls))]
-#         return all_room_df
-
-#     df = sort_keys(room_list, dataframe)
-#     ###ADDED####
-
-#     chosentile = get_provider(Vendors.OSM)
-#     palette = Category20c[10]  # PRGn[11]
-#     source = ColumnDataSource(data=df)
-#     # Define color mapper - which column will define the colour of the data points
-#     color_mapper = linear_cmap(field_name='price', palette=palette, low=df['price'].min(), high=df['price'].max())
-
-#     # Set tooltips - these appear when we hover over a data point in our map, very nifty and very useful
-#     tooltips = [("Price", "@price"), ("Region", "@neighbourhood"), ("Keywords", "@title_split"),
-#                 ("Number of Reviews", "@number_of_reviews")]
-#     # Create figure
-#     p = figure(title='Airbnb price by region in NYC', x_axis_type="mercator", y_axis_type="mercator",
-#                x_axis_label='Longitude', y_axis_label='Latitude', tooltips=tooltips)
-#     # Add map tile
-#     p.add_tile(chosentile)
-#     # Add points using mercator coordinates
-#     p.circle(x='mercator_x', y='mercator_y', color=color_mapper, source=source, size=3, fill_alpha=0.9)
-#     # Defines color bar
-#     color_bar = ColorBar(color_mapper=color_mapper['transform'],
-#                          formatter=NumeralTickFormatter(format='0.0[0000]'),
-#                          label_standoff=13, width=8, location=(0, 0))
-#     p.add_layout(color_bar, 'right')
-# #     output_notebook()
-# #     show(p)
-# #     print(len(components(p)))
-#     script1, div1= components(p)
-#     cdn_js=CDN.js_files[0]
-#     cdn_css=CDN.css_files[0]
-#     return render_template("index.html", script1=script1,div1=div1,cdn_css=cdn_css,cdn_js=cdn_js)
 
 
 # @application.route('/predict', methods=['POST'])
