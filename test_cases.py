8d6d0d982ca50baffd31eb85ebe600c2323a1aec
changliu020 <changliu020@gmail.com>
2022-06-09 22:01:40 -0700

    pytest

---
+++ test_cases.py
@@ -0,0 +1,35 @@
+from pred import predict
+from application import *
+from viz_FilterbyText.pipeline_new_1 import *
+
+
+class TestCases:
+    def test_pred(self):
+        pass
+
+    def test_get_ng_dict(self):
+        pass
+
+    def test_load_model(self):
+        pass
+
+    def test_get_pd_df(self):
+        pass
+
+    def test_parse_price_range(self):
+        pass
+
+    def test_select_from_request(self):
+        pass
+
+    def test_plot_bokeh_map_new(self):
+        pass
+
+    def test_plot_bokeh_smalldf(self):
+        pass
+
+    def test_sort_keys(self):
+        pass
+
+    def test_viz_key_df(self):
+        pass
